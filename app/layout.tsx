5315f80dbed89c7d4d90fdea95b44bd4d0f2c972
joaocardioli3895 <joaocardioli3895@gmail.com>
2025-12-23 15:39:42 -0300

    pixel meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { CartProvider } from "./_context/cart";
 import AuthProvider from "./_providers/auth";
 import { Toaster } from "sonner";
+import Image from "next/image";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -25,6 +26,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="PT-br">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s) {
+                if(f.fbq) return;
+                n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq) f._fbq=n;
+                n.push=n;
+                n.loaded=!0;
+                n.version='2.0';
+                n.queue=[];
+                t=b.createElement(e);
+                t.async=!0;
+                t.src=v;
+                s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)
+              }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1554341092432201');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <Image
+            alt="Facebook Pixel"
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1554341092432201&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className={poppins.className}>
         <AuthProvider>
           <CartProvider>{children}</CartProvider>
