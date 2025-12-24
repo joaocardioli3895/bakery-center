51fa817c944a289d84a6fec0404ec5333ae69304
joaocardioli3895 <joaocardioli3895@gmail.com>
2025-12-24 17:10:15 -0300

    meta pixel sxript 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { CartProvider } from "./_context/cart";
 import AuthProvider from "./_providers/auth";
 import { Toaster } from "sonner";
-import Image from "next/image";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -25,44 +25,42 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="PT-br">
-      <head>
-        <script
+    <html lang="pt-BR">
+      <body className={poppins.className}>
+        {/* Facebook Pixel */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s) {
-                if(f.fbq) return;
-                n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq) f._fbq=n;
-                n.push=n;
-                n.loaded=!0;
-                n.version='2.0';
-                n.queue=[];
-                t=b.createElement(e);
-                t.async=!0;
-                t.src=v;
-                s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)
-              }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/pt_BR/fbevents.js');
               fbq('init', '1554341092432201');
               fbq('track', 'PageView');
             `,
           }}
         />
+
+        {/* Fallback para usuários sem JS */}
         <noscript>
-          <Image
-            alt="Facebook Pixel"
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
             height="1"
             width="1"
             style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=1554341092432201&ev=PageView&noscript=1"
+            alt="Facebook Pixel"
           />
         </noscript>
-      </head>
-      <body className={poppins.className}>
+
         <AuthProvider>
           <CartProvider>{children}</CartProvider>
-
           <Toaster />
         </AuthProvider>
       </body>
